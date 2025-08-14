0ec8b0a8685d2a15646132d6ec813fb23289ac70
Lane Gebbie <166470962+lanedg@users.noreply.github.com>
2025-08-13 17:27:55 -0700

    Add styling for weather data display

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import "./styles.css";
 import { format } from "date-fns";
 import { getWeatherData } from "./weatherAPI/getWeatherData";
+import { addSubmitEventListener } from "./eventListeners.js/formEventListener";
 
 // async function logData() {
 //   const data = await getWeatherData("New York, NY");
 // }
 
 // logData();
+
+addSubmitEventListener();
