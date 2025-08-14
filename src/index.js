a3c4813944a8f9faf0bcb99b655ef397aa480996
Lane Gebbie <166470962+lanedg@users.noreply.github.com>
2025-08-13 19:20:10 -0700

    Fix webpack file routing issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 import { format } from "date-fns";
 import { getWeatherData } from "./weatherAPI/getWeatherData";
-import { addSubmitEventListener } from "./eventListeners.js/formEventListener";
+import { addSubmitEventListener } from "./eventListeners/formEventListener";
 
 // async function logData() {
 //   const data = await getWeatherData("New York, NY");
