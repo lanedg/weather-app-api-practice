a6ea32b0232c022a7a9fffa96739c05066961dfc
Lane Gebbie <166470962+lanedg@users.noreply.github.com>
2025-08-13 18:41:56 -0700

    Remove current temp tracking

--- src/weatherAPI/formatWeatherData.js
+++ src/weatherAPI/formatWeatherData.js
@@ -1,7 +1,6 @@
 export async function formatWeatherData(data) {
   const formattedData = {
     address: data.resolvedAddress,
-    currentTemp: data.currentConditions.temp,
     minTemp: data.days[0].tempmin,
     maxTemp: data.days[0].tempmax,
     conditions: data.days[0].conditions,
